3347f35e9092d056dc01c31947a4fd35c8ed6c14
Misthoz <maulidahmadmuafa24@gmail.com>
2025-11-24 00:15:14 +0800

    ubah general_ci;

--- db_liburan.sql
+++ db_liburan.sql
@@ -32,7 +32,7 @@ CREATE TABLE `akun` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `role` enum('Admin','User') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `akun`
